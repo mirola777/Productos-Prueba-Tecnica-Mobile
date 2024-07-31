1964a0adfee5ab02b1082f899dee4caf02f1abf5
mirola777 <jmmunoza@eafit.edu.co>
2024-07-31 16:58:21 -0500

    feat: Add DynamicForm widget for creating dynamic forms
    The commit introduces the `DynamicForm` widget, which allows the creation of dynamic forms based on a list of fields. The widget takes in a list of maps representing each field, including the field's name, type, and default value. It generates form fields dynamically based on the provided fields, including text fields, number fields, and more. The form also includes validation logic for each field type. This feature enhances code reusability and flexibility by providing a reusable widget for creating dynamic forms in the application.

---
+++ lib/presentation/widgets/forms/DynamicForm.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:productos_prueba_tecnica_mobile/util/Parser.dart';
+
+class DynamicForm extends StatefulWidget {
+  final List<Map<String, String?>> fields;
+  final Function(Map<String, String>) onSubmit;
+
+  const DynamicForm({super.key, required this.fields, required this.onSubmit});
+
+  @override
+  _DynamicFormState createState() => _DynamicFormState();
+}
+
+class _DynamicFormState extends State<DynamicForm> {
+  final _formKey = GlobalKey<FormState>();
+  final Map<String, TextEditingController> _controllers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    for (var field in widget.fields) {
+      final defaultValue = Parser.parseString(field['value'] ?? '');
+
+      _controllers[field['name']!] = TextEditingController(text: defaultValue);
+    }
+  }
+
+  @override
+  void dispose() {
+    for (var controller in _controllers.values) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          ...widget.fields.map((field) {
+            return _buildField(field);
+          }),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _submit,
+            child: const Text('Submit'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildField(Map<String, String?> field) {
+    final name = field['name']!;
+    final type = field['type']!;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: TextFormField(
+        controller: _controllers[name],
+        decoration: InputDecoration(
+          labelText: name.capitalize(),
+        ),
+        keyboardType: _getKeyboarType(type),
+        validator: (value) {
+          return _validateField(value, type);
+        },
+      ),
+    );
+  }
+
+  void _submit() {
+    if (_formKey.currentState!.validate()) {
+      final formData =
+          _controllers.map((key, value) => MapEntry(key, value.text));
+
+      widget.onSubmit(formData);
+    }
+  }
+
+  String? _validateField(String? value, String type) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a value';
+    }
+
+    if (type == 'int' && int.tryParse(value) == null) {
+      return 'Please enter a valid integer';
+    }
+
+    if (type == 'double' && double.tryParse(value) == null) {
+      return 'Please enter a valid double';
+    }
+
+    return null;
+  }
+
+  TextInputType _getKeyboarType(String type) {
+    switch (type) {
+      case 'int':
+        return const TextInputType.numberWithOptions(decimal: false);
+      case 'double':
+        return const TextInputType.numberWithOptions(decimal: true);
+      default:
+        return TextInputType.text;
+    }
+  }
+}
+
+extension StringExtension on String {
+  String capitalize() {
+    return '${this[0].toUpperCase()}${substring(1)}';
+  }
+}
