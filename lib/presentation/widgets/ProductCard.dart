84c9808e39a1cafd8d1edf179b35a2a62c44ccb6
mirola777 <jmmunoza@eafit.edu.co>
2024-07-31 16:59:07 -0500

    refactor: Update ProductCard widget to improve code reusability and flexibility
    This commit updates the `ProductCard` widget to enhance code reusability and flexibility. It introduces optional `onTap`, `onDelete`, and `onEdit` callback functions, allowing the widget to be more customizable. Additionally, the widget now uses a `GestureDetector` to handle the `onTap` event, providing better interactivity. These changes improve the overall quality of the code and make the `ProductCard` widget more versatile.

--- lib/presentation/widgets/ProductCard.dart
+++ lib/presentation/widgets/ProductCard.dart
@@ -4,42 +4,53 @@ import '../../domain/models/Product.dart';
 
 class ProductCard extends StatelessWidget {
   final Product product;
+  final Function()? onTap;
+  final Function()? onDelete;
+  final Function()? onEdit;
 
-  const ProductCard({super.key, required this.product});
+  const ProductCard(
+      {super.key,
+      required this.product,
+      this.onTap,
+      this.onDelete,
+      this.onEdit});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            ListTile(
-              title: Text(product.name),
-              subtitle: Text('\$${product.price}'),
-            ),
-            Text('Stock: ${product.stock}'),
-            const Divider(
-              height: 10,
-              color: Colors.transparent,
-            ),
-            const Text('Description:'),
-            Text(product.description ?? ''),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.edit, color: Colors.blue),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: const Icon(Icons.delete, color: Colors.red),
-                  onPressed: () {},
-                ),
-                Text(product.description ?? ''),
-              ],
-            ),
-          ],
+        child: GestureDetector(
+          onTap: onTap,
+          child: Column(
+            children: [
+              ListTile(
+                title: Text(product.name),
+                subtitle: Text('\$${product.price}'),
+              ),
+              Text('Stock: ${product.stock}'),
+              const Divider(
+                height: 10,
+                color: Colors.transparent,
+              ),
+              const Text('Description:'),
+              Text(product.description ?? ''),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.edit, color: Colors.blue),
+                    onPressed: onEdit,
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.delete, color: Colors.red),
+                    onPressed: onDelete,
+                  ),
+                  Text(product.description ?? ''),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
